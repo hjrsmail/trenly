0bc6851e86e31507977f3dd0a73714609fdfc262
Hijir Ismail <hijir512@gmail.com>
2025-07-05 11:40:49 +0800

    feat: add main.tsx as a main layout and routing

---
+++ src/main.tsx
@@ -0,0 +1,25 @@
+import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './pages/home'
+// import About from './pages/about'
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  },
+  // {
+  //   path: "/about",
+  //   element: <About />
+  // }
+])
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
+
